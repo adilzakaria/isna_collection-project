5c2ebd0c1ba352739a625f41caac0ccb771f0b84
adilzakaria <adilzakaria18@gmail.com>
2024-02-21 09:20:17 +0700

    beranda

---
+++ resources/views/frontend/navbar.blade.php
@@ -0,0 +1,28 @@
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-between">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="index.html">Isna Collection</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+          <li><a class="nav-link scrollto" href="#about">Tentang Kami</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Katalog</a></li>
+          <li class="dropdown"><a href="#"><span>Akun</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Masuk</a></li>
+              <li><a href="#">Daftar</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Hubungi Kami</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
