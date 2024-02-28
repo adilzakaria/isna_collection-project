fb00afe2491342fc53aa54d00f0183963557f2e9
adilzakaria <adilzakaria18@gmail.com>
2024-02-28 23:26:13 +0700

    asas

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -1,41 +1,72 @@
-<link href="assets/css/login.css" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="form-bg">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-offset-4 col-md-4 col-sm-offset-3 col-sm-6">
-                <div class="form-container">
-                    <div class="left-content">
-                        <h3 class="title">Site Name</h3>
-                        <h4 class="sub-title">Lorem ipsum dolor sit amet.</h4>
-                    </div>
-                    <div class="right-content">
-                        <h3 class="form-title">Login</h3>
-                        <form class="form-horizontal">
-                            <div class="form-group">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+
+    <link rel="icon" type="image/x-icon" href="../../images/favicon.ico">
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">-->
+
+    <!-- <link rel="stylesheet" href="http://jrain.oscitas.netdna-cdn.com/tutorial/css/fontawesome-all.min.css"> -->
+    <!-- <link rel="stylesheet" href="https://e6t7a8v2.stackpathcdn.com/tutorial/css/fontawesome-all.min.css"> -->
+    <link rel="stylesheet" href="../../css/fontawesome-all.min.css">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- <link rel="stylesheet" href="http://jrain.oscitas.netdna-cdn.com/tutorial/css/bootstrap.min.css"> -->
+    <!-- <link rel="stylesheet" href="https://e6t7a8v2.stackpathcdn.com/tutorial/css/bootstrap.min.css"> -->
+
+
+    <link rel="stylesheet" href="../../css/common-1.css">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600&amp;family=Poppins:wght@200;300;400;500;600&amp;display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+
+</head>
+<div class="container">
+    <div class="row">
+        <div class="col-md-offset-2 col-md-8">
+            <div class="form-container">
+                <div class="left-content">
+                    <h3 class="title" style="margin-bottom: 5px">Isna</h3>
+                    <h3 class="title" style="margin-bottom: 70px">Collection</h3>
+                    <h4 class="sub-title" style="word-break: break-all">Mewujudkan</h4>
+                    <h4 class="sub-title" style="word-break: break-all">Kreativitas</h4>
+                    <h4 class="sub-title" style="word-break: break-all">Melalui</h4>
+                    <h4 class="sub-title" style="word-break: break-all">Jarum Dan</h4>
+                    <h4 class="sub-title" style="word-break: break-all">Benang</h4>
+                </div>
+                <div class="right-content">
+                    <h3 class="form-title">Login</h3>
+                    <form class="form-horizontal">
+                        <div class="form-group">
                             <label>Username / Email</label>
-                                <input type="email" class="form-control">
-                            </div>
-                            <div class="form-group">
+                            <input type="email" placeholder="Username / Email" class="form-control" style="background-color: rgb(219, 219, 219)">
+                        </div>
+                        <div class="form-group">
                             <label>Password</label>
-                                <input type="password" class="form-control">
-                            </div>
-                            <button class="btn signin">Login</button>
-                            <div class="remember-me">
-                                <input type="checkbox" class="checkbox">
-                                <span class="check-label">Remember Me</span>
-                            </div>
-                            <a href="" class="forgot">Forgot Password</a>
-                        </form>
-                        <span class="separator">OR</span>
-                        <ul class="social-links">
-                            <li><a href=""><i class="fab fa-google"></i> Login with Google</a></li>
-                            <li><a href=""><i class="fab fa-facebook-f"></i> Login with Facebook</a></li>
-                        </ul>
-                        <span class="signup-link">Don't have an account? Sign up <a href="">here</a></span>
-                    </div>
+                            <input type="password" placeholder="Password" class="form-control" style="background-color: rgb(219, 219, 219)">
+                        </div>
+                        <button class="btn signin">Login</button>
+                        <div class="remember-me">
+                            <input type="checkbox" class="checkbox">
+                            <span class="check-label">Remember Me</span>
+                        </div>
+                        <a href="" class="forgot">Forgot Password</a>
+                    </form>
+
+                    <div class="signup-link" style="margin-top: 125px">Don't have an account? Sign up <a href="">here</a></div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+</html>
