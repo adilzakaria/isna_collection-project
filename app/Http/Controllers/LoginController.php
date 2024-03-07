de1091da01eee44d4b68bffe3fc75091e51b722a
DimasMaulanaRamadhan <dimas.maulana.2205356@students.um.ac.id>
2024-03-08 00:15:33 +0700

    login register backend

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use illuminate\support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function login()
     {
-        return view ('frontend.login');
+        return view('frontend.login', [
+            'title' => 'Login'
+        ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate(
+            [
+                'email' => 'required|email:dns',
+                'password' => 'required'
+            ],
+            [
+                'email.required' => 'Email Harus Diisi',
+                'password.required' => 'Password Harus Diisi',
+            ]
+        );
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/home');
+        }
+
+        return back()->with('LoginError', 'Login Gagal');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
--- resources/views/frontend/navbar.blade.php
+++ resources/views/frontend/navbar.blade.php
@@ -1,33 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Isna Collection</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <title>Isna Collection</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </head>
+
 <body>
-        <!-- ======= Header ======= -->
+    <!-- ======= Header ======= -->
     <header id="header" class="d-flex align-items-center">
         <div class="container d-flex justify-content-between">
 
@@ -45,17 +54,25 @@
                     <li><a class="nav-link scrollto" href="#contact">Hubungi Kami</a></li>
                     <li class="dropdown">
                         <a href="#" id="akun">
-                            <span>Akun</span>
+                            <span>Hai, {{ auth()->user()->nama }}</span>
                             <i class="bi bi-chevron-down"></i>
                             <span class="notification-badge"></span><!-- Change '3' to the actual notification count -->
                         </a>
-                        <ul>
-                            <li><a href="/akun">Akun Saya</a></li>
-                            <li><a href="#">
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a href="/akun" class="dropdown-item">Akun Saya <i class="bi bi-person-circle"></i></a></li>
+                            <li><a href="#" class="dropdown-item">
                                     <span>Pesanan</span>
-                                    <span class="notification-order"></span>
+                                    <span class="notification-order"></span><i class="bi bi-bag-fill"></i>
                                 </a></li>
-                            <li><a href="#">Keluar</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <form action="/logout" method="post">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item">Logout <i class="bi bi-box-arrow-right"></i></button>
+                                </form>
+                            </li>
                         </ul>
                     </li>
                 </ul>
@@ -64,4 +81,5 @@
         </div>
     </header><!-- End Header -->
 </body>
+
 </html>
