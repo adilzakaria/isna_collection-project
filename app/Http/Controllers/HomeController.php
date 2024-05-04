dbe9cf6447217ffbff321c5f7f80067535b751a2
orltrh <aurelyataroreh@gmail.com>
2024-05-04 20:59:16 +0700

    memperbaiki review lagi

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\ReviewRating;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        $reviews = ReviewRating::all();
-        return view ('frontend.home', compact('reviews'));
+        return view ('frontend.home');
     }
 }
